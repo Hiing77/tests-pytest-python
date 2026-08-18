d4fe013a55e78b9785c69c3f8002ffe359aa88d5
Hiing77 <supiii77@hotmail.com>
2026-08-18 09:52:07 +0200

    feat: ajout script gestion JSON avec chemins absolus

---
+++ json/gestion_json.py
@@ -0,0 +1,27 @@
+import json
+import os # 1. On importe le module pour gérer les chemins
+
+# 2. On récupère le dossier où se trouve le script
+dossier_script = os.path.dirname(os.path.abspath(__file__))
+# 3. On crée le chemin complet vers le fichier JSON
+chemin_json = os.path.join(dossier_script, "utilisateur_test.json")
+
+test = {
+    "id": "TC001",
+    "nom": "Ratta",
+    "email": "supiii77@hotmail.com",
+    "actif": False,
+}
+
+# 4. On utilise cette variable 'chemin_json' pour ouvrir le fichier
+with open(chemin_json, "w", encoding="utf-8") as fichier:
+    json.dump(test, fichier, indent=4, ensure_ascii=False)
+
+print(f"Fichier créé ici : {chemin_json}")
+
+with open(chemin_json, "r", encoding="utf-8") as fichier:
+  donnees_lues = json.load(fichier)
+  # Afficher l'email récupéré
+  print("Email récupéré du JSON :", donnees_lues["email"])
+
+
