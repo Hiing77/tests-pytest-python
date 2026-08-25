85a05602d565661f4e0564e40752edd9a5a6954e
Hiing77 <supiii77@hotmail.com>
2026-08-25 13:55:43 +0200

    Exercice

---
+++ postman/test_api_fixture_exercice3.py
@@ -0,0 +1,23 @@
+import pytest
+import requests
+
+@pytest.fixture
+def donnees_mise_a_jour():
+	return {
+    "id": 1,
+    "title": "Mon titre modifie via fixture",
+    "body": "Ceci est le nouveau contenu mis a jour pour le test QA.",
+    "userId": 1
+}
+
+def test_modifier_post(donnees_mise_a_jour):
+	url = "https://jsonplaceholder.typicode.com/posts/1"
+	reponse = requests.put(url, json=donnees_mise_a_jour)
+	
+	assert reponse.statuts_code == 200
+
+	donnees = reponse.json()
+	assert donnees["title"] == "Mon titre modifie via fixture"
+	
+
+ 
