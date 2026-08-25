04160c23c5420ae1a62a6db4e21e86b13dd7bc25
Hiing77 <supiii77@hotmail.com>
2026-08-25 14:35:55 +0200

    Exercice

---
+++ postman/test_api_parametrize_exercice.py
@@ -0,0 +1,19 @@
+import pytest
+import requests
+
+
+@pytest.mark.parametrize
+("id_post, userId_attendu" [
+    (1, 1),
+    (6, 1),
+    (11, 2)
+])
+
+def test_recuperer_post_user(id_post, userId_attendu):
+    url = f"https://jsonplaceholder.typicode.com/posts/{id_post}"
+    reponse = requests.get(url)
+
+    assert reponse.status_code == 200
+
+    donnees = reponse.json()
+    assert donnees["userId"] == userId_attendu
