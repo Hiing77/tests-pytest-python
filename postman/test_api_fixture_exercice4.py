a380aa01035ace81fc61e85ec87afca874834913
Hiing77 <supiii77@hotmail.com>
2026-08-25 14:18:35 +0200

    Exercice

---
+++ postman/test_api_fixture_exercice4.py
@@ -0,0 +1,17 @@
+import pytest
+import requests
+
+@pytest.fixture
+def id_a_supprimer():
+    return { 
+        5
+}
+
+def test_supprimer_post(id_a_supprimer):
+    url = f"https://jsonplaceholder.typicode.com/posts/){id_a_supprimer}"
+    reponse = requests.delete(url)
+
+    assert reponse.status_code == 200
+
+
+    
