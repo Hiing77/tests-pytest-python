de38c34abff397625b7441d447597a60c826f50f
Hiing77 <supiii77@hotmail.com>
2026-08-26 09:20:14 +0200

    Exercice

---
+++ postman/test_api_parametrize_exercice3.py
@@ -0,0 +1,17 @@
+import pytest
+import requests
+
+@pytest.mark.parametrize("id_photo, albumId_attendu",[
+    (1, 1),
+    (50, 1),
+    (100, 2)
+])
+
+def test_recuperer_photo(id_photo, albumId_attendu):
+    url = "(https://jsonplaceholder.typicode.com/photos/{id_photo}"
+    reponse = requests.get(url)
+
+    assert reponse.status_code == 200
+
+    donnees = reponse.json()
+    assert donnees["albumId"] == albumId_attendu
