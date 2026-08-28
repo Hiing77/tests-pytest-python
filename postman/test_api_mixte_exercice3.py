e4a083dff07a614d240a07f91b11a6571e892d96
Hiing77 <supiii77@hotmail.com>
2026-08-28 13:55:22 +0200

    Exercice

---
+++ postman/test_api_mixte_exercice3.py
@@ -0,0 +1,21 @@
+import pytest
+import requests
+
+@pytest.fixture
+def statut_attendu():
+    return 200
+
+@pytest.mark.parametrize("id_post, userId_attendu",[
+    (1, 1),
+    (10, 1),
+    (11, 2)
+])
+
+def test_recuperer_post_mixte(statut_attendu, id_post, userId_attendu):
+    url = f"https://jsonplaceholder.typicode.com/posts/{id_post}"
+    reponse = requests.get(url)
+
+    assert reponse.status_code == statut_attendu
+
+    donnees = reponse.json()
+    reponse = donnees["userId"] == userId_attendu
