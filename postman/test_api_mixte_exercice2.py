42544f44a8c8e725ff7da3c5cdca441c55477d44
Hiing77 <supiii77@hotmail.com>
2026-08-28 13:40:26 +0200

    Exercice

--- postman/test_api_mixte_exercice2.py
+++ postman/test_api_mixte_exercice2.py
@@ -6,16 +6,16 @@ def statut_attendu():
     return 200
 
 @pytest.mark.parametrize("id_album, userId_attendu",[
-(1, 1),
-(5, 1),
-(11, 2)
+    (1, 1),
+    (5, 1),
+    (11, 2)
 ])
 
-def test_recuperer_album_mixte(id_album, userId_attendu, statut_attendu)
-    url = f"https://jsonplaceholder.typicode.com/albums/){id_album}"
-    reponse = requests.post(url)
+def test_recuperer_album_mixte(statut_attendu, id_album, userId_attendu):
+    url = f"https://jsonplaceholder.typicode.com/albums/{id_album}"
+    reponse = requests.get(url)
 
-    assert reponse.status_code == 200
+    assert reponse.status_code == statut_attendu
 
     donnees = reponse.json()
     assert donnees["userId"] == userId_attendu
