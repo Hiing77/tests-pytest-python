f79f8572413a2d0c4b026e550ab5d24b47749d8b
Hiing77 <supiii77@hotmail.com>
2026-09-01 13:22:48 +0200

    Exercice

---
+++ postman/test_api_mixte_exercice5.py
@@ -0,0 +1,21 @@
+import pytest
+import requests
+
+@pytest.fixture
+def statut_attendu():
+    return 200
+
+@pytest.mark.parametrize("id_todo,completed_attendu",[
+    (1, False),
+    (2, False),
+    (4, True)
+])
+
+def test_recuperer_todo_mixte(statut_attendu, id_todo, completed_attendu):
+    url = "https://jsonplaceholder.typicode.com/todos/{id_todo}"
+    reponse = requests.get(url)
+
+    assert reponse.status_code == statut_attendu
+
+    donnees = reponse.json()
+    donnees["completed"] == completed_attendu
